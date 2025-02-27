154c80d9033b8c4755d92b53ea53947fdd0c6749
henk <barbarnorem@gmail.com>
2025-02-27 09:33:18 +0700

    Prototype blockchain

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	_"fmt"
 	"strconv"
+	"time"
+	"fmt"
 )
 
 type Block struct {
@@ -14,10 +15,48 @@ type Block struct {
 	Hash          []byte
 }
 
-func (b *Block) setHash() {
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 	hash := sha256.Sum256(headers)
 
 	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func AddGenesis() *Block {
+	return NewBlock("genesis block henkrrblaablaa", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{AddGenesis()}}
+}
+
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1 SOL to Henk")
+	bc.AddBlock("Send 2 ETH to Henk")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
