a064f37f6a110c4e409a0b886fa429e82b346e2a
henk <barbarnorem@gmail.com>
2025-02-27 08:53:45 +0700

    Prototype

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"crypto/sha256"
+	_"fmt"
+	"strconv"
+)
 
-func main() {
-	fmt.Println("hello world!")
+type Block struct {
+	TimeStamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) setHash() {
+	timestamp := []byte(strconv.FormatInt(b.TimeStamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
 }
